750de1a798140717e8607d910d62b7f35953ea86
yoga <yogaadipratama22@gmail.com>
2025-09-25 22:17:50 +0700

    Memberikan Styling Pada Routerlink Active Yang Dipilih

--- vue-router-learn/src/component/Nav.vue
+++ vue-router-learn/src/component/Nav.vue
@@ -7,8 +7,8 @@ import {RouterLink} from 'vue-router'; // import RouterLink dari vue-router
         <a href="/">Home</a>
         <a href="/about">About</a>
         |
-        <RouterLink to="/">Home</RouterLink>  <!-- RouterLink ke halaman Home -->
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink active-class="active" to="/">Home</RouterLink>  <!-- RouterLink ke halaman Home -->
+        <RouterLink active-class="active" to="/about">About</RouterLink>
 
     </div>
 </template>
@@ -20,5 +20,14 @@ div {
     padding: 10px;
     gap: 10px;
     background-color: #f2f2f2;
+}
+
+.active {
+    font-weight: 900;
+    color: white;
+    background-color: red;
+    padding: 5px 10px;
+    border-radius: 5px;
+    text-decoration: none;
 }
 </style>
