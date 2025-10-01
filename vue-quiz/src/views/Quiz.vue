73aff51555d55066aa5217083c563eef24d2770f
lordfroze <yogaadipratama22@gmail.com>
2025-10-01 15:52:34 +0700

    Menampilkan Daftar Pertanyaan Dan Pilihan Ganda

--- vue-quiz/src/views/Quiz.vue
+++ vue-quiz/src/views/Quiz.vue
@@ -2,15 +2,22 @@
 import QuizContent from '@/components/QuizContent.vue';
 import QuizHeader from '@/components/Quizheader.vue';
 import { useRoute } from 'vue-router';
+import quizes from '../data/quizes.json';
+import {ref} from 'vue';
 
 const route = useRoute();
+const quizId = parseInt(route.params.id);
+const quiz = quizes.find((q) => q.id === quizId);
+
+const currentQuestionIndex = ref(0);
+console.log(quiz);
 </script>
 
 
 <template>
 
     <QuizHeader/>
-    <QuizContent/>    
+    <QuizContent :question="quiz.questions[currentQuestionIndex]" />    
 </template>
 
 <style scoped>
